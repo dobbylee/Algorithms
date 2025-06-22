9d65081a2308905cb3892a96e6363e2ca03164bc
dobbylee <leekw1245@gmail.com>
2025-06-22 12:23:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,14 @@
+function solution(cards1, cards2, goal) {
+  while (goal.length) {
+    if (goal[0] === cards1[0]) {
+      goal.shift();
+      cards1.shift();
+    } else if (goal[0] === cards2[0]) {
+      goal.shift();
+      cards2.shift();
+    } else {
+      return "No";
+    }
+  }
+  return "Yes";
+}
