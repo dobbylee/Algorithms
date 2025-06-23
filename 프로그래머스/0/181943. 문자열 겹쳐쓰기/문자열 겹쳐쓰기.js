d2a386ed414a31465d1cfc7c7cf3ad5abbfb1b35
dobbylee <leekw1245@gmail.com>
2025-06-23 23:59:31 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,7 +1,4 @@
 function solution(my_string, overwrite_string, s) {
-    let result = [];
-    result.push(my_string.slice(0, s));
-    result.push(overwrite_string);
-    result.push(my_string.slice(s + overwrite_string.length, my_string.length));
-    return result.join('');
+    return my_string.slice(0, s) + overwrite_string
+        + my_string.slice(overwrite_string.length + s, my_string.length);
 }
