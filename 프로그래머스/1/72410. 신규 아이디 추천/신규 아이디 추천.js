13e23a597bf99eafc898e39f30d8f98bcd2eabe8
dobbylee <leekw1245@gmail.com>
2025-07-03 22:00:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,16 @@
+function solution(new_id) {
+    new_id = new_id.toLowerCase().replace(/[^\w-.]/g, "");
+    new_id = new_id.replace(/[.]+/g, ".");
+    new_id = new_id.replace(/^\.|\.$/g, "");
+    if (!new_id) new_id = "a";
+    if (new_id.length > 15) {
+        new_id = new_id.slice(0, 15);
+        if (new_id[new_id.length-1] === ".") new_id = new_id.slice(0, 14);
+    }
+    if (new_id.length < 3) {
+        while (new_id.length < 3) {
+            new_id = new_id + new_id[new_id.length-1];
+        }
+    }
+    return new_id;
+}
